52e724ffce668e0be1460af5ad604b6b6f85109c
HarukaNishida <nishidaharuka.41@gmail.com>
2021-08-29 23:45:06 +0900

    firebaseにツイートを表示

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,62 @@
 <template>
-  <div>
-    <NavBar />
-    <router-view />
+  <div class="app">
+    <button v-on:click="postTweet">ツイート</button>
+    <!-- 変更点１ -->
+    <div>
+      <p v-for="tweet in tweets" :key="tweet.id">
+        {{ tweet.text }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue"
+import firebase from "firebase"
 
 export default {
-  components: {
-    NavBar,
+  data() {
+    return {
+      /* 変更点１ */
+      tweets: [
+        // {
+        //   id: "0GwoGZuhTNhqHQDBeiVW",
+        //   text: "こんにちは、ツイートの本文です。"
+        // }
+      ],
+    }
+  },
+  methods: {
+    postTweet() {
+      /* 変更点 */
+      const tweet = {
+        text: "こんにちは、ツイートの本文です。",
+      }
+      firebase
+        .firestore()
+        .collection("tweets")
+        .add(tweet)
+        .then((ref) => {
+          this.tweets.push({
+            id: ref.id,
+            ...tweet,
+          })
+        })
+    },
+  },
+  /* 変更点２ */
+  created() {
+    firebase
+      .firestore()
+      .collection("tweets")
+      .get()
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          this.tweets.push({
+            id: doc.id,
+            ...doc.data(),
+          })
+        })
+      })
   },
 }
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-body {
-  margin: 0;
-}
-</style>
