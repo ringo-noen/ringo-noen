fbd7448673468bdac598598d45203e4aea86dc08
Ryuga <ryugasori@docomo.ne.jp>
2021-09-08 22:41:07 +0900

    検索機能完成

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    {{ allData }}
+    <input type="text" v-model="inputValue" />
+    <button v-on:click="search">検索</button>
+    <div id="result">
+      <div v-for="tweet in result" :key="tweet.text">
+        {{ tweet.username }}
+        {{ tweet.text }}
+        {{ tweet.hashtag }}
+        {{ tweet.date }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,8 +19,22 @@ import firebase from "firebase"
 export default {
   data() {
     return {
+      inputValue: "",
       allData: [],
+      result: [],
     }
+  },
+  methods: {
+    search: function () {
+      this.result = []
+      const searchword = "#" + this.inputValue
+      for (let n = 0; n < this.allData.length; n++) {
+        if (searchword === this.allData[n].hashtag) {
+          this.result.push(this.allData[n])
+        }
+      }
+      this.inputValue = ""
+    },
   },
   created() {
     {
