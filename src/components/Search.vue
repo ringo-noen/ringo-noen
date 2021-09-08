42d257fd10bec22ae4e189d008234e7200d568c5
Ryuga <ryugasori@docomo.ne.jp>
2021-09-08 21:00:49 +0900

    検索途中

---
+++ src/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ allData }}
+  </div>
+</template>
+
+<script>
+import firebase from "firebase"
+
+export default {
+  data() {
+    return {
+      allData: [],
+    }
+  },
+  created() {
+    {
+      firebase
+        .firestore()
+        .collection("tweets")
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            this.allData.push(doc.data())
+          })
+        })
+    }
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <SubmitForm />
+    <Search />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import SubmitForm from "@/components/SubmitForm.vue"
+import Search from "@/components/Search.vue"
 
 export default {
   name: "Home",
   components: {
     SubmitForm,
+    Search,
   },
 }
 </script>
