20375100b43df8ec493066634b6f9309670c2592
Ryuga <ryugasori@docomo.ne.jp>
2021-09-10 21:01:13 +0900

    Searchpage.vueでSearchを読み込む

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="home">
     <TimeLine />
-    <Search />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import TimeLine from "@/components/TimeLine.vue"
-import Search from "@/components/Search.vue"
+
 export default {
   name: "Home",
   components: {
     TimeLine,
-    Search,
   },
 }
 </script>
