5d9f4c9924d18b0ab5caa281f4d9564ae16f2428
Ryuga <ryugasori@docomo.ne.jp>
2021-09-13 13:34:49 +0900

    タイムラインと検索のcssを作りました

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,28 +1,50 @@
 <template>
-  <div class="wrap">
-    <div class="search">
-      <input
-        type="text"
-        v-model="inputValue"
-        class="form-input"
-        placeholder="どんなおにぎり？"
-      />
-      <img src="@/assets/lupe.png" class="form-button" v-on:click="search" />
+  <div class="main-content-box">
+    あ
+    <div class="main-content-out-box">
+      い
+      <div class="main-content-in-box">
+        <div class="search">
+          <input
+            type="text"
+            v-model="inputValue"
+            class="form-input"
+            placeholder="どんなおにぎり？"
+          />
+          <img
+            src="@/assets/lupe.png"
+            class="form-button"
+            v-on:click="search"
+          />
 
-      <div class="result">
-        <div v-for="tweet in result" :key="tweet.text">
-          <img v-bind:src="tweet.fileURL" class="tweet_image" />
-          {{ tweet.username }}
-          {{ tweet.text }}
-          {{ tweet.place }}
-          {{ tweet.hashtag }}
-          {{ tweet.date }}
+          <div class="result">
+            <div v-for="tweet in result" :key="tweet.text">
+              <img v-bind:src="tweet.fileURL" class="tweet_image" />
+
+              <ul class="tweettext">
+                <li>
+                  {{ tweet.username }}
+                </li>
+                <li>
+                  {{ tweet.text }}
+                </li>
+                {{
+                  tweet.place
+                }}
+                <li>
+                  {{ tweet.hashtag }}
+                </li>
+                <li>
+                  {{ tweet.datetime }}
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-
 <script>
 import firebase from "firebase"
 
@@ -62,15 +84,66 @@ export default {
 }
 </script>
 <style>
+.main-content-box {
+  width: 100vw;
+  height: 100%;
+  min-height: 65vh;
+  /* width: 10vw;
+  height: 65vh; */
+  background-image: url("~@/assets/赤玉模様.png");
+  background-repeat: repeat;
+  background-size: 20%;
+  /* background-color: red; */
+  /* background-size: cover; */
+}
+.main-content-out-box {
+  width: 80vw;
+  height: 100%;
+  min-height: 65vh;
+  margin-left: 10vw;
+  background-color: black;
+}
+.main-content-in-box {
+  width: 70vw;
+  height: 100%;
+  min-height: 65vh;
+  margin-left: 5vw;
+  background-color: rgb(112, 190, 160);
+}
 .form-input {
-  background-color: red;
+  display: block;
+  margin: auto;
+  background-color: #cccccc;
+  color: black;
+  width: 20vw;
+  height: 3vw;
+}
+.form-input::placeholder {
+  color: black;
 }
 .tweet_image {
-  width: 100px;
-  height: 100px;
+  display: block;
+  margin: auto;
+  width: 25vw;
+  height: 25vw;
+  border: solid 10px #000000;
+}
+ul {
+  list-style: none;
+}
+.tweettext {
+  text-align: center;
+  padding: 0.5em 1em;
+  margin: 2em 0;
+  color: #5d627b;
+  background: white;
+  border-top: solid 5px #5d627b;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.22);
 }
 .form-button {
-  width: 4vw;
-  height: 4vw;
+  display: block;
+  margin: auto;
+  width: 5vw;
+  height: 5vw;
 }
 </style>
--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,14 +1,34 @@
+
 <template>
+  <div class="main-content-box">
+    あ
+    <div class="main-content-out-box">
+      い
+      <div class="main-content-in-box">
   <div class="form__wrapper">
-    <div>
+    <div class="text-content">
       <p v-for="tweet in tweets" :key="tweet.id">
-        {{ tweet.username }}
         <img v-bind:src="tweet.fileURL" class="tweet_image" />
-        {{ tweet.text }}
-        {{ tweet.place }}
-        {{ tweet.hashtag }}
-        {{ tweet.datetime }}
+
+        <ul class="tweettext">
+          <li>
+          {{ tweet.username }}
+          </li>
+        <li>
+          {{ tweet.text }}
+          </li>
+          {{ tweet.place }}
+          <li>
+          {{ tweet.hashtag }}
+          </li>
+          <li>
+          {{ tweet.datetime }}
+          </li>
+        </ul>
       </p>
+    </div>
+  </div>
+    </div>
     </div>
   </div>
 </template>
@@ -55,11 +75,66 @@ export default {
 </script>
 
 <style scoped>
+.main-content-box {
+  width: 100vw;
+  height: 100%;
+  min-height: 65vh;
+  /* width: 10vw;
+  height: 65vh; */
+  background-image: url("~@/assets/赤玉模様.png");
+  background-repeat: repeat;
+  background-size: 20%;
+  /* background-color: red; */
+  /* background-size: cover; */
+}
+.main-content-out-box {
+  width: 80vw;
+  height: 100%;
+  min-height: 65vh;
+  margin-left: 10vw;
+  background-color: black;
+}
+.main-content-in-box {
+  width: 70vw;
+  height: 100%;
+  min-height: 65vh;
+  margin-left: 5vw;
+  background-color: rgb(112, 190, 160);
+}
 .form__wrapper {
   padding: 1rem;
 }
+.text-content {
+  
+  padding-left: 70px;
+  margin-bottom: 50px;
+   
+}
+
+.tweettext {
+text-align: center;
+padding: 0.5em 1em;
+  margin: 2em 0;
+  color: #5d627b;
+  background: white;
+  border-top: solid 5px #5d627b;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.22);
+
+
+  
+ 
+}
+
 .tweet_image {
-  width: 100px;
-  height: 100px;
+  display: block;
+     margin:auto;
+     width:25vw;
+     height: 25vw;
+      border: solid 10px #000000;
+
+     
+     
+
+
 }
 </style>
