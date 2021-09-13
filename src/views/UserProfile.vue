810962f6473a8a79012be5f0a9fde15586d7232a
Sorakumagai <sora.1999312@icloud.com>
2021-09-13 20:36:08 +0900

    ユーザ情報のcssをやりました

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,28 +1,39 @@
 <template>
-  <div user-profile>
-    <h1>ユーザ詳細</h1>
-    <div class="signed-in-user-profile__photo">
-      <img
-        :src="user.photoURL ? user.photoURL : 'https://via.placeholder.com/150'"
-      />
+  <div class="main-content-box">
+    <div class="main-content-out-box">
+      <div class="main-content-in-box">
+        <div class="user-profile-box">
+          <div class="user-profile">
+            <div class="signed-in-user-profile__photo">
+              <img
+                :src="
+                  user.photoURL
+                    ? user.photoURL
+                    : 'https://via.placeholder.com/150'
+                "
+              />
+            </div>
+            <div class="signed-in-user-profile" v-if="user">
+              <table class="">
+                <tr>
+                  <td>名前</td>
+                  <td>{{ user.displayName }}</td>
+                </tr>
+                <tr>
+                  <td>ID</td>
+                  <td>{{ user.uid }}</td>
+                </tr>
+                <tr>
+                  <td>email</td>
+                  <td>{{ user.email }}</td>
+                </tr>
+              </table>
+            </div>
+            <div v-else>No User Signed In</div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="signed-in-user-profile" v-if="user">
-      <table class="">
-        <tr>
-          <td>名前</td>
-          <td>{{ user.displayName }}</td>
-        </tr>
-        <tr>
-          <td>ID</td>
-          <td>{{ user.uid }}</td>
-        </tr>
-        <tr>
-          <td>email</td>
-          <td>{{ user.email }}</td>
-        </tr>
-      </table>
-    </div>
-    <div v-else>No User Signed In</div>
   </div>
 </template>
 
@@ -48,9 +59,39 @@ export default {
 </script>
 
 <style scoped>
+.main-content-box {
+  width: 100vw;
+  height: 100%;
+  min-height: 65vh;
+  /* width: 10vw;
+  height: 65vh; */
+  background-image: url("~@/assets/赤玉模様.png");
+  background-repeat: repeat;
+  background-size: 20%;
+  /* background-color: red; */
+  /* background-size: cover; */
+}
+.main-content-out-box {
+  width: 80vw;
+  height: 100%;
+  min-height: 65vh;
+  margin-left: 10vw;
+  background-color: black;
+}
+.main-content-in-box {
+  width: 70vw;
+  height: 100%;
+  min-height: 65vh;
+  margin-left: 5vw;
+  background-color: rgb(112, 190, 160);
+}
+.user-profile-box {
+  width: 80%;
+  margin-left: 10%;
+}
 .user-profile {
   width: 90%;
-  margin: 0 auto;
+  padding: 5%;
 }
 .signed-in-user-profile__photo {
   text-align: center;
